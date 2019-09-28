f85c7237c395313bb151347fa4fc8e1120b09808
rahul <rahulchaurasia02@gmail.com>
2019-09-28 18:54:31 +0530

    admin page set

--- emp_api/emp_app/admin.py
+++ emp_api/emp_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from emp_app.models import Emp
+
 
 # Register your models here.
+admin.site.register (Emp)
--- emp_api/emp_app/models.py
+++ emp_api/emp_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Emp(models.Model):
+    name=models.CharField(max_length=50)
+    eid=models.IntegerField(max_length=10)
+    email=models.EmailField(max_length=50)
+    salery=models.DecimalField(max_digits=20 , decimal_places=2)
+    def __str__(self):
+        return self.name
