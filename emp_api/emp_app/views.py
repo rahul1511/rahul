55dfa7e67847a852779634dcf4005f30adbd1f16
rahul1511 <55529052+rahul1511@users.noreply.github.com>
2019-09-29 23:50:03 +0530

    views updated with the all curd operations

--- emp_api/emp_app/views.py
+++ emp_api/emp_app/views.py
@@ -24,3 +24,32 @@ class Emplist(APIView):
             return Response(Slst.data, status=status.HTTP_201_OK)
         else:
             return Response(Slst.error, status=status.HTTP_400_BAD_REQUEST)
+
+# now create another class based view for the modifiction
+class Emp_modify(APIView):
+    # create a method the retive the object for the selection
+    def get_object(self,id):
+        #try to check the object is available in data base or not
+        try:
+            return Emp.objects.get(id)
+        except:
+            return Response({"error":"object not found", status=status.HTTP_404_NOT_FOUND})
+    def put(self,request,id):
+        lst=self.get_object(id)
+        #serialize that element
+        Slst=Emp_serializer(lst,data=request.data)
+        # chect whethet the data modified is valid or not
+        if Slst.is_valid():
+            Slst.save()
+            return Response(Slst.data,ststus=status.HTTP_200_OK)
+        else:
+            return Response(Slst.error,status=status.HTTP_404_BAD_REQUEST)
+    # create another method to delete the data
+    def delete(self,request,id):
+        lst=self.get_object(id)
+        lst.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        
+                            
+            
