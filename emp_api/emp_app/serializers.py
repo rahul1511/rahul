b494ef11df49719f8b3ce7f650a276a77d153c02
rahul1511 <55529052+rahul1511@users.noreply.github.com>
2019-09-30 10:11:19 +0530

    serializers created

--- emp_api/emp_app/serializers.py
+++ emp_api/emp_app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from emp_app.models import Emp
+
 # create a serializer file that erilize the modeldata in the database
 class Emp_serializer(serializers.ModelSerializer):
     class Meta:
